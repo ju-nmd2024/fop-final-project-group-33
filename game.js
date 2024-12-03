ddc368cc1764204aba07feb6195c41c79019daaa
Essi0888 <saes24js@student.ju.se>
2024-12-03 13:55:28 +0100

    Update game.js

--- game.js
+++ game.js
@@ -9,8 +9,7 @@ let columns = 6;
 let brickWidth;
 let brickHeight = 30;
 let spaceBetweenBricks = 10;
-let spaceFromTopWall = 50;
-let spaceFromLeftWall = 50;
+let spaceFromWall = 50;
 
 // Variables for paddle and ball
 let paddle;
@@ -39,20 +38,25 @@ class Brick {
 function setup() {
   createCanvas(800, 600);
 
+<<<<<<< Updated upstream
   width = 800;
   height = 600;
 
+=======
+  console.log(width);
+  console.log(height);
+>>>>>>> Stashed changes
 
   brickWidth =
-    (width - spaceFromLeftWall * 2 - spaceBetweenBricks * (columns - 1)) /
+    (width - spaceFromWall * 2 - spaceBetweenBricks * (columns - 1)) /
     columns;
 
   // grid of bricks
   for (let row = 0; row < rows; row++) {
     for (let column = 0; column < columns; column++) {
       // Calculate the x and y position for the current brick
-      let x = spaceFromLeftWall + column * (brickWidth + spaceBetweenBricks);
-      let y = spaceFromTopWall + row * (brickHeight + spaceBetweenBricks);
+      let x = spaceFromWall + column * (brickWidth + spaceBetweenBricks);
+      let y = spaceFromWall + row * (brickHeight + spaceBetweenBricks);
 
       // Create a new Brick object at the calculated position
       bricks.push(new Brick(x, y));
@@ -62,19 +66,30 @@ function setup() {
   // Create paddle
   paddle = {
     x: width / 2 - paddleWidth / 2,   //Ensures the paddle starts centered horizontally in canvas
+<<<<<<< Updated upstream
     y: height - 50,
+=======
+    y: height - 100,                  //Position of the paddle vertically
+>>>>>>> Stashed changes
     width: paddleWidth,
     height: paddleHeight,
-    speed: 5            //Controls how fast the paddle moves
+    speed: 10                          //Controls how fast the paddle moves
   }; 
       
 
   // Creat ball
   ball = {
+<<<<<<< Updated upstream
     x: width / 2,     //Horizontal position. (in the center)
     y: height / 1.5,    //Vertical position. (in the center)
     dx: 5,            //Left and right movement speed. Positive mean to the right
     dy: -5,           //Up and down movement speed. Negative means upward
+=======
+    x: width - 200,     //Horizontal position. (in the center)
+    y: height / 2,    //Vertical position. (in the center)
+    dx: 3,            //Left and right movement speed. Positive mean to the right
+    dy: -3,           //Up and down movement speed. Negative means upward
+>>>>>>> Stashed changes
     size: ballSize
   };
   
@@ -97,11 +112,15 @@ function draw() {
   background(0, 0, 0);
   starfield();
 
+
+  console.log(width);
+  console.log(height);
+
   //Move paddle
-  if (keyIsDown(LEFT_ARROW) && paddle.x > 0) {
+  if (keyIsDown(LEFT_ARROW) && paddle.x > spaceFromWall) {
     paddle.x -= paddle.speed;
   }
-  if (keyIsDown(RIGHT_ARROW) && paddle.x < width - paddle.width) {
+  if (keyIsDown(RIGHT_ARROW) && paddle.x < width - paddle.width - spaceFromWall) {
     paddle.x += paddle.speed;
   }
 
@@ -113,6 +132,7 @@ function draw() {
   ball.x += ball.dx;
   ball.y += ball.dy;
 
+<<<<<<< Updated upstream
   // When ball hits side walls
 
   if (ball.x - ball.size / 2 <= 0 || ball.x + ball.size / 2 >= width) {
@@ -132,6 +152,13 @@ function draw() {
   ) {
     ball.dy = ball.dy * -1; 
   }
+=======
+  if(ball.x == spaceFromWall || ball.x == (width - spaceFromWall)) {
+    ball.x = - ball.x;
+    ball.y = ball.y;
+  }
+
+>>>>>>> Stashed changes
   
   //Draw ball
   fill(255, 0, 0);
